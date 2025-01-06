e34c2176d287684a2b98c29f39dffac4b78aa0c3
gaoshiyao <gaoshiyao@wodcloud.com>
2025-01-06 18:52:28 +0800

    s3 patch

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -4,6 +4,7 @@ git config --global --add safe.directory $PWD
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple/
 
 set -ex
+git apply .beagle/s3.patch
 
 if ! [ -e $PWD/.venv/bin/activate ]; then
   python3 -m venv $PWD/.venv
