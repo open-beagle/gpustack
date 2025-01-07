464f813ff6369d22c9b8bb4dcd2b808cfa7f60bd
shucheng <shucheng@bd-apaas.com>
2025-01-07 09:35:18 +0800

    v0.4.1

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -4,7 +4,6 @@ git config --global --add safe.directory $PWD
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple/
 
 set -ex
-git apply .beagle/s3.patch
 
 if ! [ -e $PWD/.venv/bin/activate ]; then
   python3 -m venv $PWD/.venv
@@ -15,6 +14,11 @@ if $(git diff --quiet gpustack/cmd/download_tools.py); then
   git apply .beagle/v0.4.1-logginglocal.patch
 fi
 
+if $(git diff --quiet gpustack/worker/backends/base.py); then
+  git apply .beagle/v0.4.1-s3.patch
+fi
+
 make build
 
 git apply -R .beagle/v0.4.1-logginglocal.patch
+git apply -R .beagle/v0.4.1-s3.patch
