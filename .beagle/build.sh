e2ddfae144338edcd943263a45192c8697e7846b
gaoshiyao <gaoshiyao@bd-apaas.com>
2025-07-29 10:59:57 +0800

    v0.7.0

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -11,19 +11,14 @@ if ! [ -e $PWD/.venv/bin/activate ]; then
 fi
 
 if $(git diff --quiet pyproject.toml); then
-  git apply .beagle/v0.4.1-s3-project.patch
-fi
-
-if $(git diff --quiet gpustack/worker/backends/base.py); then
-  git apply .beagle/v0.4.1-s3.patch
+  git apply .beagle/v0.7.0-s3-project.patch
 fi
 
 if $(git diff --quiet gpustack/cmd/download_tools.py); then
-  git apply .beagle/v0.4.1-logginglocal.patch
+  git apply .beagle/v0.7.0-s3-logging.patch
 fi
 
 make build
 
-git apply -R .beagle/v0.4.1-s3-project.patch
-git apply -R .beagle/v0.4.1-s3.patch
-git apply -R .beagle/v0.4.1-logginglocal.patch
+git apply -R .beagle/v0.7.0-s3-project.patch
+git apply -R .beagle/v0.7.0-s3-logging.patch
