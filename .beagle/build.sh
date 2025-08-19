da0b3be54daa4370d2fe7a1c0f993a46b6d08d46
gaoshiyao <gaoshiyao@bd-apaas.com>
2025-08-19 19:58:42 +0800

    修改路径

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -11,19 +11,19 @@ if ! [ -e $PWD/.venv/bin/activate ]; then
 fi
 
 if $(git diff --quiet pyproject.toml); then
-  git apply .beagle/v0.4.1-s3-project.patch
+  git apply .beagle/v0.4.2-s3-project.patch
 fi
 
 if $(git diff --quiet gpustack/worker/backends/base.py); then
-  git apply .beagle/v0.4.1-s3.patch
+  git apply .beagle/v0.4.2-s3.patch
 fi
 
 if $(git diff --quiet gpustack/cmd/download_tools.py); then
-  git apply .beagle/v0.4.1-logginglocal.patch
+  git apply .beagle/v0.4.2-logginglocal.patch
 fi
 
 make build
 
-git apply -R .beagle/v0.4.1-s3-project.patch
-git apply -R .beagle/v0.4.1-s3.patch
-git apply -R .beagle/v0.4.1-logginglocal.patch
+git apply -R .beagle/v0.4.2-s3-project.patch
+git apply -R .beagle/v0.4.2-s3.patch
+git apply -R .beagle/v0.4.2-logginglocal.patch
